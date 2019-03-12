7c4e1f2c7c6f0832e4b0ed9834af607860d6fb42
jormora <39917627+jormora@users.noreply.github.com>
2019-03-12 17:39:36 -0500

    Add files via upload

---
+++ SeleccionPersonalizadoScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class SeleccionPersonalizadoScript : MonoBehaviour {
+
+    public static int filas = 10;
+    public static int columnas = 10;
+
+    public GameObject TextoFila;
+    public GameObject TextoColumna;
+
+    public void UpdateSeleccion(BaseEventData eventData){
+        TextoFila.GetComponent<TextMesh>().text = SeleccionPersonalizadoScript.filas + "";
+        TextoColumna.GetComponent<TextMesh>().text = SeleccionPersonalizadoScript.columnas + "";
+    }
+    
+}
